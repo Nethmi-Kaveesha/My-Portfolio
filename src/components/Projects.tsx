d968a4702008ed81593e3df20d767a9c914be555
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-09-22 22:22:44 -0700

    done

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -32,7 +32,7 @@ const projects: ProjectProps[] = [
             "Frontend of UTAX platform for managing tax-related workflows. Built with React and deployed on Vercel.",
         liveLink: "https://utax-frontend.vercel.app/login",
         githubLink: "#", // If no public GitHub repo
-        image: "public/Screenshot 2025-06-22 030820.png", // Add a screenshot path here
+        image: "src/assets/Screenshot 2025-06-22 030820.png", // Add a screenshot path here
     },
     {
         title: "E-Commerce Web App",
